2e850ba03640d6f500c4b333f841eb95c87c1bda
Guillaume Gicquel <guillaume.gicquel@yahoo.fr>
2018-02-11 10:50:24 +0100

    tryin to create a package

---
+++ app/views.py
@@ -0,0 +1,26 @@
+from IMIBotInterface import app
+from flask import Flask, jsonify, request, render_template
+
+from ControlsHandler import ControlsHandler
+
+@app.route('/')
+def home():
+    return render_template('home.html', test=request.base_url)
+
+
+@app.route('/config')
+def config():
+    return render_template('config.html')
+
+
+@app.route('/_move')
+def move():
+    angle = request.args.get('angle', 0, type=float)
+    strength = request.args.get('strength', 0, type=float)
+    ControlsHandler.move(angle, strength)
+    return jsonify(angle, strength)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
+
